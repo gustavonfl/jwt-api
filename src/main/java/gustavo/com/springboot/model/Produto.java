cae4cf6d896c60147788e4e79eae43845a9847f7
Gustavo Leite <qustavoleite@gmail.com>
2026-04-27 15:53:37 -0300

    feat: adiciona o model de produto

---
+++ src/main/java/gustavo/com/springboot/model/Produto.java
@@ -0,0 +1,36 @@
+package gustavo.com.springboot.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "produtos")
+public class Produto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private Double preco;
+
+    public Produto(){}
+
+    public Produto(String nome, Double preco){
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+}
