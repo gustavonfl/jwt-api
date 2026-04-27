ab90299ec460b291efbadbd1574f8a8bc47298be
Gustavo Leite <qustavoleite@gmail.com>
2026-04-27 15:54:59 -0300

    feat: adiciona o repository de produto

---
+++ src/main/java/gustavo/com/springboot/repository/ProdutoRepository.java
@@ -0,0 +1,9 @@
+package gustavo.com.springboot.repository;
+
+import gustavo.com.springboot.model.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+}
