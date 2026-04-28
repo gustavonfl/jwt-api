5d5cece3f0b1a2e90b0e889784016cbea9b76c96
Gustavo Leite <qustavoleite@gmail.com>
2026-04-28 08:25:55 -0300

    chore: adiciona as dependencias spring securtity, jwt e jackson

--- src/main/java/gustavo/com/springboot/controller/ProdutoController.java
+++ src/main/java/gustavo/com/springboot/controller/ProdutoController.java
@@ -1,7 +1,9 @@
 package gustavo.com.springboot.controller;
 
+import gustavo.com.springboot.exceptions.RecursoNaoEncontradoException;
 import gustavo.com.springboot.model.Produto;
 import gustavo.com.springboot.service.ProdutoService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -23,10 +25,9 @@ public class ProdutoController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Produto> buscarProduto(@PathVariable Long id){
-        return produtoService.buscarPorId(id)
-                .map(ResponseEntity::ok)
-                .orElse(ResponseEntity.notFound().build());
+    public ResponseEntity<?> buscarProduto(@PathVariable Long id){
+        Produto produto = produtoService.buscarPorId(id);
+        return ResponseEntity.ok(produto);
     }
 
     @PostMapping
