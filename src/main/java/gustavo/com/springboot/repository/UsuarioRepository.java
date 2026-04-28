3e9b6d3d8fb058d90ba5372f283d7f277f38fd64
Gustavo Leite <qustavoleite@gmail.com>
2026-04-28 08:35:38 -0300

    feat: cria o repository de usuario com metodo para buscar por username

---
+++ src/main/java/gustavo/com/springboot/repository/UsuarioRepository.java
@@ -0,0 +1,9 @@
+package gustavo.com.springboot.repository;
+
+import gustavo.com.springboot.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    Optional<Usuario> findByUsername(String username);
+}
